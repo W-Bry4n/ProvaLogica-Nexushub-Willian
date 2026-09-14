521ad7dddb5a404f1cea1391d5a6609f8d6838e5
W-Bry4n <willianbryan307@gmail.com>
2026-09-14 12:50:39 +0000

    Etapa 2 + correção Etapa 1

--- main.py
+++ main.py
@@ -6,4 +6,11 @@ Startup = {
 }
 
 solucao_ativa = ["Firewall", "IA", "Scan de Vulnerabilidades"]
-print("Startup:", Startup["nome"], "Segmento:", Startup["segmento"])
+print("Startup:", Startup["nome"], "Segmento:", Startup["segmento"], "Produto:", solucao_ativa[0])
+
+# Etapa 2: Mapeamento das Bancadas de Trabalho (Matriz 2D)
+bancadas = ([1,0],
+            [0,1])
+print("Bancada N1:", bancadas[0][0], "Bancada N2:", bancadas[0][1], "Bancadas N3:", bancadas[1][0], "Bancada N4:", bancadas[1][1])
+print("1 = Ocupado e 0 = Livre")
+           
