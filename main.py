aa35cec0b5535491f065ca20b17cf30574f5456a
W-Bry4n <willianbryan307@gmail.com>
2026-09-14 12:38:17 +0000

    Etapa 1

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+# Etapa 1: Cadastro da Startup e Projetos
+Startup = {
+    "nome": "CyberPulse Tech",
+    "segmento": "Segurança da Informação",
+    "ano_adesao": "2026"
+}
 
+solucao_ativa = ["Firewall", "IA", "Scan de Vulnerabilidades"]
+print("Startup:", Startup["nome"], "Segmento:", Startup["segmento"])
